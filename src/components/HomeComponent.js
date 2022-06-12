a9b55ed0255981cd6d88225902ce1110993e83ac
matthewgstillman <matthewgstillman@gmail.com>
2022-06-11 20:05:47 -0600

    Latest update

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,4 @@
-import '../css/main.css';
+import "../css/main.css";
 import NavbarComponent from "./NavbarComponent";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import profilePic from "../Images/profilePic.jpg";
@@ -7,21 +7,36 @@ const HomeComponent = () => {
   return (
     <div>
       <NavbarComponent />
-      <Image
+      <div className="container">
+        <Image
           fluid
           data-testid="homeHeaderImage"
           className="home-component-image"
           src={profilePic}
-      /> 
+        />
         <div className="about-paragraph__container">
-              <h1>About</h1>
-              <p className="about-parahraph">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. 
-              No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is 
-              pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses
-              to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
+          <h1>About</h1>
+          <p className="about-parahraph">
+            But I must explain to you how all this mistaken idea of denouncing
+            pleasure and praising pain was born and I will give you a complete
+            account of the system, and expound the actual teachings of the great
+            explorer of the truth, the master-builder of human happiness. No one
+            rejects, dislikes, or avoids pleasure itself, because it is
+            pleasure, but because those who do not know how to pursue pleasure
+            rationally encounter consequences that are extremely painful. Nor
+            again is there anyone who loves or pursues or desires to obtain pain
+            of itself, because it is pain, but because occasionally
+            circumstances occur in which toil and pain can procure him some
+            great pleasure. To take a trivial example, which of us ever
+            undertakes laborious physical exercise, except to obtain some
+            advantage from it? But who has any right to find fault with a man
+            who chooses to enjoy a pleasure that has no annoying consequences,
+            or one who avoids a pain that produces no resultant pleasure?"
+          </p>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default HomeComponent;
--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -3,21 +3,33 @@ import { Container, Row, Col, Image } from "react-bootstrap";
 import NavbarComponent from "./NavbarComponent";
 import "../css/main.css";
 import profilePic from "../Images/profilePic.jpg";
+import omdb from "../Images/omdbProjectScreenshot.png";
 
 const ProjectsComponent = () => {
   return (
     <div>
       <NavbarComponent/>
-      <div class="flip-card" tabIndex="0">
-      <div class="flip-card-inner">
-        <div class="flip-card-front">
-          <img src={profilePic} alt="" />
-        </div>
-        <div class="flip-card-back">
-          <h3>Whoaaa!!!</h3>
-        </div>
-      </div>
-    </div>
+        <div class="flip-card" tabIndex="0">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <img src={omdb} height="380" />
+            </div>
+            <div class="flip-card-back">
+              <a href="https://main.d22bh3gff8l7ye.amplifyapp.com/">OMDB API Project</a>
+            </div>
+          </div>
+          <br />
+          <div class="flip-card-2" tabIndex="0">
+          <div class="flip-card-2-inner">
+            <div class="flip-card-2-front">
+              <img src={omdb} height="380" />
+            </div>
+            <div class="flip-card-2-back">
+              <a href="https://main.d22bh3gff8l7ye.amplifyapp.com/">OMDB API Project</a>
+            </div>
+          </div>
+       </div>
+       </div>
     </div>
   );
 };
