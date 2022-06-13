7ff96df9b59d8a8971af6cf7d72078ead00c341e
matthewgstillman <matthewgstillman@gmail.com>
2022-06-12 20:42:08 -0600

    Updated

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -1,35 +1,50 @@
 import React from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
 import NavbarComponent from "./NavbarComponent";
 import "../css/main.css";
-import profilePic from "../Images/profilePic.jpg";
 import omdb from "../Images/omdbProjectScreenshot.png";
 
 const ProjectsComponent = () => {
   return (
     <div>
-      <NavbarComponent/>
-        <div class="flip-card" tabIndex="0">
-          <div class="flip-card-inner">
-            <div class="flip-card-front">
-              <img src={omdb} height="380" />
-            </div>
-            <div class="flip-card-back">
-              <a href="https://main.d22bh3gff8l7ye.amplifyapp.com/">OMDB API Project</a>
-            </div>
+      <NavbarComponent />
+      <div class="flip-card" tabIndex="0">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img src={omdb} height="380" />
           </div>
-          <br />
-          <div class="flip-card-2" tabIndex="0">
+          <div class="flip-card-back">
+            <a href="https://main.d22bh3gff8l7ye.amplifyapp.com/">
+              OMDB API Project
+            </a>
+          </div>
+        </div>
+        <h3>OMDB Movie API</h3>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+        <br />
+        <br />
+        <div class="flip-card-2" tabIndex="0">
           <div class="flip-card-2-inner">
             <div class="flip-card-2-front">
               <img src={omdb} height="380" />
             </div>
             <div class="flip-card-2-back">
-              <a href="https://main.d22bh3gff8l7ye.amplifyapp.com/">OMDB API Project</a>
+              <a href="https://main.d22bh3gff8l7ye.amplifyapp.com/">
+                OMDB API Project
+              </a>
             </div>
           </div>
-       </div>
-       </div>
+        </div>
+      </div>
     </div>
   );
 };
